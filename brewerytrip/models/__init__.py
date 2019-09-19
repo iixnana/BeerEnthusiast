6903c66a79dfcfbe623b1c27a9069de7796ab9b2
kamnan <kamile.nanartonyte@ktu.edu>
2019-09-19 19:32:27 +0300

    General models logic

--- brewerytrip/models/__init__.py
+++ brewerytrip/models/__init__.py
@@ -7,12 +7,7 @@ from haversine import haversine as distance
 __all__ = ['Brewery', 'Beer', 'Geocode']
 
 
-def hello():
-    print("This is a function")
-
-
 def print_list(point, dis):
     list = Brewery.objects.filter_breweries(point, dis)
-    for val in list:
-        print(val.name)
     print(len(list))
+    return list
